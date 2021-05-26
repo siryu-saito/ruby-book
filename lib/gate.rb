37e5100dfbedf2230a6e2159aeb967fb041f0116
siryu-saito <saito.s.ryu@gmail.com>
2021-05-26 16:30:36 +0900

    [Finish]改札機プログラムの作成

---
+++ lib/gate.rb
@@ -0,0 +1,28 @@
+class Gate
+  STATIONS = [:umeda, :juso, :mikuni]
+  FARES = [150, 190]
+
+  def initialize(name)
+    @name = name
+  end
+
+  def enter(ticket)
+    ticket.stamp(@name)
+  end
+
+  def exit(ticket)
+    true
+  end
+
+  def calc_fare(ticket)
+    from = STATIONS.index(ticket.stamped_at)
+    to = STATIONS.index(@name)
+    distance = to - from
+    FARES[distance - 1]
+  end
+
+  def exit(ticket)
+    fare = calc_fare(ticket)
+    fare <= ticket.fare
+  end
+end
