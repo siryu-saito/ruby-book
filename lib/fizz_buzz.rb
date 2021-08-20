3fcf0a337c381196b8603187eebadb28ff804bc1
siryu-saito <saito.s.ryu@gmail.com>
2021-08-20 22:07:39 +0900

    [Retry]Fizz_Buzz

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,11 +1,25 @@
+# 再復習
 def fizz_buzz(n)
   if n % 15 == 0
-    "FizzBuzz"
+    'FizzBuzz'
   elsif n % 3 == 0
-    "Fizz"
+    'Fizz'
   elsif n % 5 == 0
-    "Buzz"
+    'Buzz'
   else
     n.to_s
   end
 end
+
+puts fizz_buzz(1)
+puts fizz_buzz(2)
+puts fizz_buzz(3)
+puts fizz_buzz(4)
+puts fizz_buzz(5)
+puts fizz_buzz(6)
+puts fizz_buzz(7)
+puts fizz_buzz(10)
+puts fizz_buzz(11)
+puts fizz_buzz(12)
+puts fizz_buzz(15)
+puts fizz_buzz(30)
