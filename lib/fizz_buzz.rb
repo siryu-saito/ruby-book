8cf78807565dddff4b63927a154678f12d4fbe9d
siryu-saito <saito.s.ryu@gmail.com>
2021-08-22 22:11:41 +0900

    [Retry]Fizz_Buzz test

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -10,16 +10,3 @@ def fizz_buzz(n)
     n.to_s
   end
 end
-
-puts fizz_buzz(1)
-puts fizz_buzz(2)
-puts fizz_buzz(3)
-puts fizz_buzz(4)
-puts fizz_buzz(5)
-puts fizz_buzz(6)
-puts fizz_buzz(7)
-puts fizz_buzz(10)
-puts fizz_buzz(11)
-puts fizz_buzz(12)
-puts fizz_buzz(15)
-puts fizz_buzz(30)
